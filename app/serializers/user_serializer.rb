72122f4f68c138eefd3a8fa8a823aa7702829e8e
AnansiOmega <ignasbutautas7@gmail.com>
2021-02-19 16:09:59 -0600

    added password to serializer so that auth can work smoothly

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,23 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :fname, :lname, :age, :gender, :climbing_preference, :commitment, :skill_level, :bio, :street, :city, :state, :photo
+  attributes :id, :username, :fname, :lname, :age, :gender, :climbing_preference, :commitment, :skill_level, :bio, :street, :city, :state, :photo, :password
 
 
 
   include Rails.application.routes.url_helpers
   
   def photo
-      rails_blob_path(object.image, only_path: true) if object.photo.attached?
+      rails_blob_path(object.photo, only_path: true) if object.photo.attached?
   end
 end
-# t.string "username"
-# t.string "fname"
-# t.string "lname"
-# t.integer "age"
-# t.string "sex"
-# t.string "climbing_preference"
-# t.integer "commitment"
-# t.string "skill_level"
-# t.text "bio"
-# t.string "street"
-# t.string "city"
-# t.string "state"
