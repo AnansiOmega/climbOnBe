5a02f67214c470d0283c613ec619d47c258075bf
AnansiOmega <ignasbutautas7@gmail.com>
2021-02-22 15:27:07 -0600

    added geo location to users, going to work on finding users near eachother

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
     has_one_attached :photo
+    geocoded_by :address
+    after_validation :geocode
     has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
     has_many :followees, through: :followed_users
     has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
     has_many :followers, through: :following_users
+
+    def address
+        [street, city, state].compact.join(', ')
+    end
 end
